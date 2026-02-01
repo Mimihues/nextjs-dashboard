552e9270f2db8fd93e34eee3731eb64c5fd8f8e8
hola <miriamhsantos@gmail.com>
2026-02-01 13:59:59 +0100

    Corregido Streaming: ahora el SideNav carga de forma independiente

--- app/ui/revenue-chart.tsx
+++ app/ui/revenue-chart.tsx
@@ -1,23 +1,17 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
-
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+import { fetchRevenue } from '@/app/lib/data';
+ 
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue();
   const chartHeight = 350;
-
-  // Lógica para generar las etiquetas del eje Y (activada para el Cap. 7)
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
-  // Si no hay datos, mostramos un mensaje (activada para evitar errores visuales)
+ 
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
-
+ 
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -25,7 +19,6 @@ export default async function RevenueChart({
       </h2>
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          {/* Etiquetas del eje Y */}
           <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
@@ -34,8 +27,6 @@ export default async function RevenueChart({
               <p key={label}>{label}</p>
             ))}
           </div>
-
-          {/* Barras de la gráfica */}
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
